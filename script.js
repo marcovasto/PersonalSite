916b750d9fbf73ce196e089702a25fe076a8c1bd
Marco <marcovasto@icloud.com>
2025-03-06 13:24:42 +0100

    checked texts, alternative image descriptions, correct functioning of links, code and class names

--- script.js
+++
@@ -1,8 +0,0 @@
-// Seleziona gli elementi necessari
-const toggleButton = document.querySelector('.navbar-toggle');
-const menu = document.querySelector('.navbar-menu');
-
-// Aggiungi un evento al clic del pulsante
-toggleButton.addEventListener('click', () => {
-    menu.classList.toggle('active'); // Mostra/nasconde il menu
-});
