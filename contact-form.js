3a5004c8908a199eefd6cd750cd6691997d1c701
Marco <marcovasto@icloud.com>
2025-03-17 12:25:48 +0100

    Moved JS to a dedicated file, modified external links to improve security

---
+++ contact-form.js
@@ -0,0 +1,22 @@
+// Load EmailJS SDK
+document.addEventListener("DOMContentLoaded", function () {
+    emailjs.init("zXO943iHRLZoMONd9"); // Public Key
+
+    // Managing form submission
+    document.getElementById("contact-form").addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent page reload
+
+        // Send form with EmailJS
+        emailjs.sendForm("service_nh7ajlr", "template_kbpevfj", this)
+            .then(function () {
+                document.getElementById("success-message").style.display = "block";
+                document.getElementById("error-message").style.display = "none";
+                document.getElementById("contact-form").reset(); // Clear form
+            })
+            .catch(function (error) {
+                document.getElementById("error-message").style.display = "block";
+                document.getElementById("success-message").style.display = "none";
+                console.error("Failed to send message:", error);
+            });
+    });
+});
